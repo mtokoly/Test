1125d04178d94e67c63da532cbff26e110a3dd5c
Matt Tokoly <mdt2349@dhcp-146-6-163-136.util.cpfm.utexas.edu>
2014-07-09 08:49:31 -0500

    6/9 Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,30 @@
 
 $('#menulink').on('click', function()
 {
-	$('#menulink, #menu').toggleClass('active');
+	if ( $('body').hasClass('menu-regular-min') )
+	{
+		$('body').toggleClass('menu-regular-min-active menu-regular-min-inactive');
+	}
+	else if ( $('body').hasClass('menu-regular') )
+	{
+		$('body').toggleClass('menu-regular-active menu-regular-inactive');
+	}
+	else if ( $('body').hasClass('menu-overlay') )
+	{
+		$('body').toggleClass('menu-overlay-active menu-overlay-inactive');
+	}
 });
 
+$(window).resize(function() { menu_screensize() });
+function menu_screensize()
+{
+	if ( $(window).width() < 1240)
+	{
+		$('body').addClass('menu-regular-min');
+	}
+	else
+	{
+		$('body').removeClass('menu-regular-min');
+	}
+}
+menu_screensize();
--- index.php
+++ index.php
@@ -6,35 +6,41 @@
     <meta name="viewport" content="width=1024">
     <link rel="stylesheet" href="./assets/css/normalize.css" />
     <link rel="stylesheet" href="./assets/css/styles.css" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
-<body>
+<body class="menu-regular menu-regular-active menu-regular-min-inactive">
 
-<div id="topbar">
+<div id="topbar-wrap"><div id="topbar">
+	<a href="javascript:void(0)" id="menulink"><span class="bars"></span><span class="arrow"></span></a>
+	<a href="/" id="logo">RIPPIN'</a>
+</div></div>
+<!-- [END] Topbar -->
+
+<div id="menu-wrap"><div id="menu">
+</div></div>
+<!-- [END] Menu -->
+
+<div id="container">
+	<div id="content-wrap"><div id="content">
+		blah
+	</div></div>
+	<!-- [END] Content -->
 </div>
+<!-- [END] Container -->
 
-<a href="javascript:void(0)" id="menulink">
-	<span></span>
-</a>
-
-<div id="menu">
-</div>
-
-<div id="main">
-	<div id="content"></div>
-	<div id="footer">
-		<p>&copy; <?php echo date('Y'); ?> Example</p>
-		<ul>
-			<li><a href="#" class="accent">Accented</a></li>
-			<li><a href="#">Blog</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">Terms</a></li>
-			<li><a href="#">Privacy</a></li>
-			<li><a href="#">Copyright</a></li>
-		</ul>
-	</div>
-</div>
+<div id="footer-wrap"><div id="footer">
+	<p>&copy; <?php echo date('Y'); ?> Example</p>
+	<ul>
+		<li><a href="#" class="accent">Accented</a></li>
+		<li><a href="#">Blog</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Contact</a></li>
+		<li><a href="#">Terms</a></li>
+		<li><a href="#">Privacy</a></li>
+		<li><a href="#">Copyright</a></li>
+	</ul>
+</div></div>
+<!-- [END] Footer -->
 
 <script src="./assets/js/scripts.js"></script>
 </body>
